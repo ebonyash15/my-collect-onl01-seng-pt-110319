f3b8a346aa9cdd9ca05da021c9ad796c63d99b73
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-09 01:07:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  count = 0
+  updated_array = []
+  while count < asrray.size
+    updated_array << yield(array[count])
+    count += 1    
+  end
+  updated_array
+end
 
-
+ 
